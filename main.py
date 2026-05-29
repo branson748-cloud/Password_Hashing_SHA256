9c315a4b708f47449162d7e588563858131df1bc
Branson <bransonneo@gmail.com>
2026-05-29 07:37:05 +0000

    Initial Code

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import hashlib
 
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+def check_password(stored_hash, user_password):
+    return stored_hash == hash_password(user_password)
+
+# Demo
+password = input("Create password: ")
+stored_hash = hash_password(password)
+
+print("\nHashed Password:")
+print(stored_hash)
+
+login = input("\nEnter password to login: ")
+
+valid_input = False
+
+while not valid_input:
+    if check_password(stored_hash, login):
+        print("Access granted")
+        valid_input = True
+    else:
+        print("\nWrong password, please try again")
+        login = input("\nEnter password to login: ")
